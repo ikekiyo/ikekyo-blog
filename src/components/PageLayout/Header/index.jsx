b15ed541068225e84b3e60917af60dfc68e24381
ikekiyo <ike.kyo0802@gmail.com>
2021-03-17 21:50:36 +0900

    change routing

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -31,16 +31,11 @@ export default () => {
       <Layout className={`${style.navWrap} ${menu ? null : style.hidden} ${menu ? style.openMenu : null}`}>
         <div className={style.backgroundDiv}>
           <ul className={style.nav}>
-            <li className={style.navItem}>
+            {/* <li className={style.navItem}>
               <Link to="/" onClick={toggleMenu} activeClassName={style.anchorActive}>
                 About
               </Link>
-            </li>
-            <li className={style.navItem}>
-              <Link to="/contact" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Contact
-              </Link>
-            </li>
+            </li> */}
             <li className={style.navItem}>
               <Link to="/blog" onClick={toggleMenu} activeClassName={style.anchorActive}>
                 Blog
@@ -50,12 +45,7 @@ export default () => {
               <Link to="/tags" onClick={toggleMenu} activeClassName={style.anchorActive}>
                 Tags
               </Link>
-            </li>
-            <li className={style.navItem}>
-              <Link to="/resume" onClick={toggleMenu} activeClassName={style.anchorActive}>
-                Resume
-              </Link>
-            </li>
+          </li>
           </ul>
         </div>
       </Layout>
